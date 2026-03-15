aa3ca39290356f72de6aa4b5a2f014c5bb15fdd8
akshitpuniadev <akshitpunia.dev@gmail.com>
2026-03-15 15:45:31 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,16 @@
+# --- MISSION 24: DAY 1 ---
+# Harry Bhai Video 1-3 Summary
+
+# 1. Print Function (Computer ki awaaz)
+print("Aaj maine coding shuru ki!")
+
+# 2. Modules (Bani-banayi machines)
+import os 
+# 'os' ek internal module hai jo phone ke folders dikhata hai.
+print("Folders check ho gaye, module samajh aa raha hai.")
+
+# 3. Pip (Delivery Boy)
+# Note: Pip se 'pandas' mangwane ke liye plugin chahiye hota hai.
+# Abhi mujhe uski zaroorat nahi hai, basic is best!
+
+print("30% Pip aur 40% Module samajh aaya, baaki aage dekhlenge!")
