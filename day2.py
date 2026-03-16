cb7f2861a33856249079ca97c6de225fa0f1b579
akshitpuniadev <akshitpunia.dev@gmail.com>
2026-03-16 13:26:16 +0530

    Create day2.py

---
+++ day2.py
@@ -0,0 +1,25 @@
+# ==========================================
+# MISSION 24: DAY 2 - Python Mastery
+# Topics: Print, Sep, End, Escape Sequences
+# ==========================================
+
+# 1. Print and Calculations
+print("--- My Day 2 Practice ---")
+print(15 * 17) # Simple calculation
+print("15 * 17") # Printing as a string (quotes ka khel)
+
+# 2. Escape Sequences (\n)
+# Ek line ko do hisson mein todna
+print("thie is my ball\nand your ball buy is tommorow")
+
+# 3. Separator (sep) and End (end)
+# sep: cheezon ke beech mein kya aayega
+# end: line ke khatam hone par kya aayega
+print("akshit", 7, 8, sep="~", end="009")
+print(" akshit shop") 
+
+# 4. Comments (The '#' power)
+# Ye line Python nahi padhega, ye sirf mere liye hai.
+# Aaj bukhar tha phir bhi Video 4 aur 5 nipta di! 🚀
+
+print("\nDay 2 Summary: Concept of quotes, sep, and end are clear!")
