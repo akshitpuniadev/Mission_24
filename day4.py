cad448ec69d8d942f8d2c7deb97ab7cacc61bfa5
akshitpuniadev <akshitpunia.dev@gmail.com>
2026-03-18 12:41:22 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,21 @@
+# Day 9: Typecasting Example
+a = "10" # String
+b = "20" # String
+print("Without Typecasting:", a + b) # Output: 1020
+
+# Explicit Typecasting
+sum_val = int(a) + int(b)
+print("With Typecasting:", sum_val) # Output: 30
+
+# Day 10: User Input Example
+name = input("Enter your name: ")
+print("Hello " + name)
+
+# Simple Calculator Input
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
